0da1fd7c1ce5412082eccb22225124e925ed8042
guimatera <guilhermematera@ejcm.com.br>
2023-08-05 20:06:38 -0300

    feature: creating tests for user CRUD in sucess cases with mocks.

--- src/__tests__/controllers/userController.test.ts
+++ src/__tests__/controllers/userController.test.ts
@@ -1,28 +1,174 @@
-import request from 'supertest';
-import app from '../../index';
-import { faker } from '@faker-js/faker';
+import { Request, Response } from "express";
+import { faker } from "@faker-js/faker";
+import userController from "../../controllers/userController";
 
 interface User {
-  Name: String;
-  Email: String;
+  id: Number
+  name: String;
+  email: String;
+  "profile": {
+    id: Number,
+    userId: Number
+  }
 }
 
+let expectedUser: User = {
+  id: 1,
+  name: faker.string.alpha(10),
+  email: faker.internet.email(),
+  "profile": {
+    id: 1,
+    userId: 1
+  }
+};
+
+let updatedUser: User = {
+  id: expectedUser.id,
+  name: faker.string.alpha(10),
+  email: faker.internet.email(),
+  "profile": {
+    id: expectedUser.profile.id,
+    userId: expectedUser.profile.userId
+  }
+};
+
+describe("Create User Tests", () => {
+
+  it("Should create a user sucessfully", async () => {
+
+    const req: Partial <Request> = {
+      body:{
+        Name: expectedUser.name,
+        Email: expectedUser.email
+      }
+    };
+  
+    const res: Partial <Response> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    }
+
+    await userController.Create(req as Request, res as Response);
+
+    expect(res.status).toHaveBeenCalledWith(201);
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith(expectedUser);
+
+  });
+
+});
+
+describe("Find One User Tests", () => {
+
+    it("Should find a user Successfully", async () => {
+
+      const req: Partial <Request> = {
+          params: {
+            userId: String(expectedUser.id)
+          }
+      };
+
+      const res: Partial <Response> = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn()
+      }
+
+      await userController.findOne(req as Request, res as Response);
+
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledTimes(1);
+      expect(res.json).toHaveBeenCalledWith(expectedUser);
+    });
+
+});
+
+describe("Find All Users Tests", () => {
+
+  it("Should find all users Successfully", async () => {
+
+    const req: Partial <Request> = {};
+
+    const res: Partial <Response> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    }
+
+    await userController.findAll(req as Request, res as Response);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith([expectedUser]);
+  });
+
+});
+
+describe("Update One User Tests", () => {
+
+  it("Should update a User Successfully", async () => {
+
+    const req: Partial <Request> = {
+        params: {
+          userId: String(expectedUser.id)
+        },
+        body: {
+          Name: updatedUser.name,
+          Email: updatedUser.email
+        }
+    };
+
+    const res: Partial <Response> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    }
+
+    await userController.Update(req as Request, res as Response);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith(updatedUser);
+  });
+
+});
+
+describe("Delete One User Tests", () => {
+
+  it("Should delete a User Successfully", async () => {
+
+    const req: Partial <Request> = {
+        params: {
+          userId:  String(updatedUser.id)
+        }
+    };
+
+    const res: Partial <Response> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    }
+
+    await userController.Destroy(req as Request, res as Response);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith(updatedUser);
+  });
+
+});
+/*
 describe('Create User Tests', () => {
   let userId: Number;
-
   let User: User = {
     Name: faker.person.firstName(),
     Email: faker.internet.email()
   };
 
-  it('HTTP Status 200 - Working Fine', async () => {
+  it('HTTP Status 201 - Working Fine', async () => {
     const Response = await request(app).post('/User').send(User);
     expect(Response.body).toMatchObject({
       id: expect.any(Number),
       name: User.Name,
       email: User.Email
     });
-    expect(Response.statusCode).toBe(200);
+    expect(Response.statusCode).toBe(201);
 
     userId = Response.body.id;
   });
@@ -103,6 +249,17 @@ describe('Find All Users Tests', () => {
       await request(app).delete(`/User/${userIds[i - 1]}`);
     }
   });
+
+  it('HTTP Status 404 - No records found', async () => {
+    const Response = await request(app).get(`/Users`);
+    expect(Response.body.length == 0).toBe(true);
+    expect(Response.statusCode).toBe(404);
+  });
+
+  it('HTTP Status 500 - Internet Server Error', async () => {
+    const Response = await request(app).get('/Users');
+    expect(Response.statusCode).toBe(500);
+    });
 });
 
 describe('Find One User Tests', () => {
@@ -161,7 +318,6 @@ describe('Update One User Tests', () => {
         userId: userId
       }
     };
-
     const Response = await request(app).put(`/User/${userId}`).send(updatedUser);
     expect(Response.body).toMatchObject(retrievedUser);
     expect(Response.statusCode).toBe(200);
